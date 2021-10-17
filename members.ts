2070ee59ec2c7648117811d0936a4f35267ce77d
BTBF <37224549+btbf@users.noreply.github.com>
2021-10-17 12:03:26 +0400

    Update members.ts

--- members.ts
+++ members.ts
@@ -27,5 +27,23 @@ export const members: Member[] = [
     avatarSrc: "https://static.adapools.org/pool_logo/1d2972246d8adda98836626a34e337525f5206e552715d28379b5fdb.png",
     sources: ["https://coffeepool.jp/feed/"],
     twitterUsername: "hix_coffeepool",
-  }
+  },
+    {
+    id: "MYB",
+    name: "MIYABI KYOTO",
+    role: "SPO",
+    bio: "SPO",
+    avatarSrc: "https://static.adapools.org/pool_logo/de1d84fcbacbbc51ee5511b371c621a5335ce94d5d5d720d4c066107.png",
+    sources: ["https://miyabi-kyoto.org/feed/"],
+    twitterUsername: "Miyabi_Kyoto_",
+  },
+      {
+    id: "WYAM",
+    name: "WYAM-StakePool",
+    role: "SPO",
+    bio: "SPO",
+    avatarSrc: "https://static.adapools.org/pool_logo/940d6893606290dc6b7705a8aa56a857793a8ae0a3906d4e2afd2119.png",
+    sources: ["https://wyam-stakepool.com/?feed=rss2"],
+    twitterUsername: "WYAM_StakePool",
+  },
 ];
