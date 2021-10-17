50448a79688f96245b040f5dc91648646c073296
BTBF <37224549+btbf@users.noreply.github.com>
2021-10-17 09:48:34 +0400

    Update members.ts

--- members.ts
+++ members.ts
@@ -11,12 +11,21 @@ export const members: Member[] = [
     twitterUsername: "btbfpark",
   },
   {
-    id: "amanda",
-    name: "Amanda",
-    role: "Frontend dev",
-    bio: "Frontend developer,",
-    avatarSrc: "/avatars/amanda.jpg",
-    sources: ["https://qiita.com/catnose99/feed.atom"],
-    twitterUsername: "catnose99",
+    id: "EXPO",
+    name: "EXPO POOL",
+    role: "SPO",
+    bio: "SPO",
+    avatarSrc: "https://static.adapools.org/pool_logo/b352298733374ae760475bb3cd86b4b52fe3dcf7fd61f678e969030e.png",
+    sources: ["https://expo-dow.com/feed/"],
+    twitterUsername: "Dow_shi1",
   },
+    {
+    id: "COFFEE",
+    name: "COFFEE POOL",
+    role: "SPO",
+    bio: "SPO",
+    avatarSrc: "https://static.adapools.org/pool_logo/1d2972246d8adda98836626a34e337525f5206e552715d28379b5fdb.png",
+    sources: ["https://coffeepool.jp/feed/"],
+    twitterUsername: "hix_coffeepool",
+  }
 ];
