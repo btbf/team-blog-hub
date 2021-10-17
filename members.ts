78157b66d87327455351a7555b5ae9433172a878
BTBF <37224549+btbf@users.noreply.github.com>
2021-10-17 12:18:59 +0400

    Update members.ts

--- members.ts
+++ members.ts
@@ -55,4 +55,13 @@ export const members: Member[] = [
     sources: ["https://sugarstakepool.jp/feed/"],
     twitterUsername: "sugar417K",
   },
+   {
+    id: "SAKE",
+    name: "Sake Pool",
+    role: "SPO",
+    bio: "SPO",
+    avatarSrc: "https://static.adapools.org/pool_logo/37d2b23f574680a88198e99cc884b6cf9292ce3500467584cb5093f2.png",
+    sources: ["https://sakepool.com/feed.xml"],
+    twitterUsername: "sakepool_ada",
+  },
 ];
