cf9c7d5e0aa2b8cc2a346f1b4faedd81f286738c
BTBF <37224549+btbf@users.noreply.github.com>
2021-11-02 13:54:06 +0400

    Update ScrollableMembers.tsx

--- src/components/ScrollableMembers.tsx
+++ src/components/ScrollableMembers.tsx
@@ -1,35 +1,12 @@
 import Link from "next/link";
 import { members } from "@members";
 import { getMemberPath } from "@src/utils/helper";
-var members1 = members.slice(0, 10)
-var members2 = members.slice(11, 20)
+//var members1 = members.slice(0, 10)
+//var members2 = members.slice(11, 20)
 export const ScrollableMembers: React.FC = () => {
   return (
     <div className="scrollable-members"> 
-      {members1.map((member, i) => (
-        <Link
-          key={`scrollable-member-${i}`}
-          href={getMemberPath(member.id)}
-          passHref
-        >
-          <a className="scrollable-member__link">
-            <span className="scrollable-member__image">
-              <img
-                src={member.avatarSrc}
-                alt={member.name}
-                className="scrollable-member__img"
-                width={80}
-                height={80}
-              />
-            </span>
-            <span className="scrollable-member__name">{member.name}</span>
-            <span className="scrollable-member__role">{member.role}</span>
-          </a>
-        </Link>
-      ))}
-    </div>
-    <div className="scrollable-members"> 
-      {members2.map((member, i) => (
+      {members.map((member, i) => (
         <Link
           key={`scrollable-member-${i}`}
           href={getMemberPath(member.id)}
