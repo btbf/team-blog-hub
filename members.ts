e9f2759b4b1a1cb462fd1f53ef6c5334e5e21a69
BTBF <37224549+btbf@users.noreply.github.com>
2022-06-28 17:52:42 +0400

    Update members.ts

--- members.ts
+++ members.ts
@@ -61,7 +61,7 @@ export const members: Member[] = [
     role: "SPO",
     bio: "SPO",
     avatarSrc: "https://static.adapools.org/pool_logo/5c8751066db31d6745bc8a58ba1534de67a95f9962fc309b4673dd8c.png",
-    sources: ["https://note.com/cardano_sugar/rss"],
+    sources: ["https://note.com/cardano_sugar/m/m14e3ecc96dd2/rss"],
     twitterUsername: "sugar417K",
   },
    {
