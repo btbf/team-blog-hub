903707959c5f74000239203c6edf726bc8f25950
BTBF <37224549+btbf@users.noreply.github.com>
2022-11-01 22:45:01 +0400

    Update members.ts

--- members.ts
+++ members.ts
@@ -10,13 +10,4 @@ export const members: Member[] = [
     sources: ["https://xstakepool.com/feed/"],
     twitterUsername: "btbfpark",
   },
-{
-    id: "DOW",
-    name: "DOW Pool",
-    role: "SPO",
-    bio: "SPO",
-    avatarSrc: "https://static.adapools.org/pool_logo/b352298733374ae760475bb3cd86b4b52fe3dcf7fd61f678e969030e.png",
-    sources: ["https://expo-dow.com/feed/"],
-    twitterUsername: "Dow_shi1",
-  },
 ];
