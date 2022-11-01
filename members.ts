46eab9c1a3d5df9771dfbec3a84da69a6a00066b
BTBF <37224549+btbf@users.noreply.github.com>
2022-11-01 22:32:42 +0400

    Update members.ts

--- members.ts
+++ members.ts
@@ -91,13 +91,4 @@ export const members: Member[] = [
     sources: ["https://www.youtube.com/feeds/videos.xml?channel_id=UCz3QzD1qX5fm1KqQGGZhR0A"],
     twitterUsername: "necotaSensei",
   },
-   {
-    id: "ISSA",
-    name: "ISSA@Catalyst提案者",
-    role: "Youtube",
-    bio: "Youtube",
-    avatarSrc: "https://pbs.twimg.com/profile_images/1472110623748403206/Ygxq3OiP_400x400.jpg",
-    sources: ["https://www.youtube.com/feeds/videos.xml?channel_id=UCTuKVZSUCtZX6HxK-Dg4eNw"],
-    twitterUsername: "Cardano_ISSA",
-  },
 ];
